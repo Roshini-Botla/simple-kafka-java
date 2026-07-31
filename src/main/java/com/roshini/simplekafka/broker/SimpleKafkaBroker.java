4420e9786b96d4612d92382a829ac03218c07b28
Roshini Botla <roshinibotla12@gmail.com>
2026-07-31 10:59:48 +0530

    Add SimpleKafkaProducer client; verified end-to-end producer-to-broker message delivery

--- src/main/java/com/roshini/simplekafka/broker/SimpleKafkaBroker.java
+++ src/main/java/com/roshini/simplekafka/broker/SimpleKafkaBroker.java
@@ -1,8 +1,12 @@
 package com.roshini.simplekafka.broker;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import com.roshini.simplekafka.protocol.Message;
+import com.roshini.simplekafka.protocol.MessageSerializer;
 
 public class SimpleKafkaBroker {
 
@@ -19,6 +23,27 @@ public class SimpleKafkaBroker {
         while (true) {
             Socket clientSocket = serverSocket.accept();
             System.out.println("New client connected: " + clientSocket.getInetAddress());
+            handleClient(clientSocket);
+        }
+    }
+
+    private void handleClient(Socket clientSocket) {
+        try {
+            DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+
+            int length = in.readInt();
+            byte[] data = new byte[length];
+            in.readFully(data);
+
+            Message message = MessageSerializer.deserialize(data);
+
+            System.out.println("Received message -> Topic: " + message.getTopic()
+                    + ", Key: " + message.getKey()
+                    + ", Value: " + new String(message.getValue()));
+
+            clientSocket.close();
+        } catch (IOException e) {
+            System.out.println("Error handling client: " + e.getMessage());
         }
     }
 
---
+++ src/main/java/com/roshini/simplekafka/client/SimpleKafkaProducer.java
@@ -0,0 +1,31 @@
+package com.roshini.simplekafka.client;
+
+import com.roshini.simplekafka.protocol.Message;
+import com.roshini.simplekafka.protocol.MessageSerializer;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class SimpleKafkaProducer {
+
+    public static void main(String[] args) throws IOException {
+        String host = "localhost";
+        int port = 9092;
+
+        Message message = new Message("orders", 0, "user1", "Hello from producer!".getBytes(), System.currentTimeMillis());
+
+        try (Socket socket = new Socket(host, port)) {
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            byte[] data = MessageSerializer.serialize(message);
+
+            out.writeInt(data.length);
+            out.write(data);
+            out.flush();
+
+            System.out.println("Message sent successfully.");
+        }
+    }
+
+}
